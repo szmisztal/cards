89a600b14db8e76b1a605946fc512c25f7e1d8b6
Szymon Misztal <sz.misztal@gmail.com>
2023-03-15 14:06:33 +0100

    add faker and create_contacts function

--- cards.py
+++ cards.py
@@ -1,3 +1,7 @@
+from faker import Faker
+fake = Faker()
+
+
 class BaseContact():
     def __init__(self, name, surname, phone, email):
         self.name = name 
@@ -27,8 +31,22 @@ class BussinesContact(BaseContact):
     
     def comp_contact(self):
         print(f"Wybieram numer służbowy: {self.bussines_phone} i dzwonię do {self.name} {self.surname} z firmy {self.company}")
-        
 
+    @property
+    def name_length(self):
+        return len(self.name + self.surname)
+    
+def create_contacts(card_type, amount):
+    contacts = []
+    for i in range(amount):
+        if card_type == "base":
+            contact = BaseContact(name = fake.first_name(), surname = fake.last_name(), phone = fake.phone_number(), email = fake.email())
+        elif card_type == "bussines":
+            contact = BussinesContact(name = fake.first_name(), surname = fake.last_name(), phone = fake.phone_number(), email = fake.email(), profession = fake.job(), company = fake.company(), bussines_phone = fake.number_phone())
+        else:
+            print("Wrong card type.")
+        contacts.append(contact)
+    return contacts
 
 card_1 = BussinesContact(name = "Szymon", surname = "Misztal", phone = "123456789", email = "sz.misztal@gmail.com", profession = "Driver", company = "Orlen", bussines_phone = "987654321")
 card_2 = BussinesContact(name = "Tomasz", surname = "Łysoń", phone = "123789456", email = "t.łysoń@gmail.com", profession = "IT Specialist", company = "Fujitsu", bussines_phone = "654987321")
@@ -36,6 +54,7 @@ card_3 = BussinesContact(name = "Piotr", surname = "Lauk", phone = "456789123", 
 card_4 = BussinesContact(name = "Radosław", surname = "Misztal", phone = "456123789", email = "r.misztal@gmail.com", profession = "Cook", company = "SoloPizza", bussines_phone = "987321654")
 card_5 = BussinesContact(name = "Jagoda", surname = "Sarzyńska", phone = "789123456", email = "j.sarzynska@gmail.com", profession = "Personnel Manager", company = "BNP", bussines_phone = "654321987")
 cards = [card_1, card_2, card_3, card_4, card_5]
-for card in cards:
-    print(card)
+
+
+
 
